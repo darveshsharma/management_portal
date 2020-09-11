33cbe5dea136eea1890b08f74efb0ea1e86554ce
bobby <darvesh.07.ds>
2020-09-11 21:16:44 +0530

    new commit

--- accounts/views/employee.py
+++ accounts/views/employee.py
@@ -18,22 +18,14 @@ class employee_register(CreateView):
 
 
 def EmployeeCreateLeave(request):
-    print("1st line")
     form = LeaveForm(request.user, request.POST or None)
-    print("1st line")
     if request.method == 'POST':
-        print("1st line")
         if form.is_valid():
-            print("1st line")
             print('Saved form')
-            print("1st line")
             form.save(request.user)
-            print("1st line")
             form =LeaveForm(request.user)
-            print("1st line")
     context = {
         'form': form,
         'user': request.user,
     }
-    print("1st line")
     return render(request, 'leave_form.html',context)
